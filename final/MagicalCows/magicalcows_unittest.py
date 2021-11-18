448fbef744790e46da0b8239eb94a337f9ed81d5
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2021-11-18 10:48:26 -0700

    Finish magical cows, update readme

--- final/MagicalCows/magicalcows_unittest.py
+++ final/MagicalCows/magicalcows_unittest.py
@@ -21,18 +21,26 @@ class MagicalCowsTest(unittest.TestCase):
         self.assertEqual(check_day(dp, max_cows, 19), 0, "No cows are on the farm.")
         self.assertEqual(check_day(dp, max_cows, 50), 0, "No cows are on the farm.")
     
-    def test_no_cows(self):
-        max_cows = 5
-        cows_per_farm = [0, 0, 0, 0, 0]
+    def test_initial_one_cow(self):
+        max_cows = 1
+        cows_per_farm = [1]
         dp = init_dp(max_cows)
         init_farm_counts(dp, cows_per_farm)
         calculate_all_day_counts(dp, max_cows)
-        self.assertEqual(check_day(dp, max_cows, 0), 0, "No cows are on the farm.")
-        self.assertEqual(check_day(dp, max_cows, 12), 0, "No cows are on the farm.")
-        self.assertEqual(check_day(dp, max_cows, 19), 0, "No cows are on the farm.")
-        self.assertEqual(check_day(dp, max_cows, 50), 0, "No cows are on the farm.")
-
+        self.assertEqual(check_day(dp, max_cows, 0), 1, "There is initially 1 cow.")
+        self.assertEqual(check_day(dp, max_cows, 12), 4096, "There are 2^12 cows.")
+        self.assertEqual(check_day(dp, max_cows, 19), 524288, "There are 2^19 cows.")
+        self.assertEqual(check_day(dp, max_cows, 50), 1125899906842624, "There are 2^50 cows.")
         
+    
+    def test_initial_many_cows(self):
+        max_cows = 5
+        cows_per_farm = [1, 2, 4, 1, 4]
+        dp = init_dp(max_cows)
+        init_farm_counts(dp, cows_per_farm)
+        calculate_all_day_counts(dp, max_cows)
+        self.assertEqual(check_day(dp, max_cows, 0), 5, "There is initially 5 cows.")
+        self.assertEqual(check_day(dp, max_cows, 1), 7, "There are 7 cows on the second day")
    
         
 if __name__ == "__main__":
---
+++ final/README.md
@@ -0,0 +1,51 @@
+# Project 2 - Kattis Problems
+
+### I choose problems with difficulties of 4.9, 4.6, and 4.4. Furthermore, each problem has at least 3 automated test cases and uses the unittest library. All the criteria is met, so I believe that I earn a score of 100%. Here are the links to the problems on Open Kattis: [Longest Common Substring](https://open.kattis.com/problems/longestcommonsubstring), [CD](https://open.kattis.com/problems/cd), and [Magical Cows](https://open.kattis.com/problems/magicalcows).
+
+<br/>
+
+## Problem #1 - [Longest Common Substring](https://open.kattis.com/problems/longestcommonsubstring) (4.9 Difficulty)
+<br/>
+
+
+![alt text](longestcommonsubstring/unit_tests.png)
+
+<br/>
+
+![alt text](longestcommonsubstring/kattis_tests.png)
+
+<br/>
+
+![alt text](longestcommonsubstring/kattis_accepted.png)
+
+<br/>
+
+## Problem #2 - [CD](https://open.kattis.com/problems/cd) (4.6 Difficulty)
+<br/>
+
+![alt text](CD/unit_tests.png)
+
+<br/>
+
+![alt text](CD/kattis_test.png)
+
+<br/>
+
+![alt text](CD/kattis_accepted.png)
+
+<br/>
+
+## Problem #3 - [Magical Cows](https://open.kattis.com/problems/magicalcows) (4.4 Difficulty)
+<br/>
+
+![alt text](MagicalCows/unit_tests.png)
+
+<br/>
+
+![alt text](MagicalCows/kattis_tests.png)
+
+<br/>
+
+![alt text](MagicalCows/kattis_accepted.png)
+
+<br/>
